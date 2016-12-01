e70152c91eae9ddcfea783b86f38186a206bfccf
Six <brbsix@gmail.com>
2016-12-01 01:18:04 -0800

    Initial commit for WSJ redirector

---
+++ The Wall Street Journal Redirector.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         The Wall Street Journal Redirector
+// @namespace    https://brbsix.github.io/
+// @version      0.1
+// @description  Bypass The Wall Street Journal paywall by redirecting to an archive of the page
+// @author       Brian Beffa <brbsix@gmail.com>
+// @match        http://www.wsj.com/articles/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function getMetaContent(name) {
+        return document.querySelector('meta[name="' + name + '"]').content;
+    }
+
+    if (getMetaContent('article.access') !== 'free' || getMetaContent('article.template') !== 'full') {
+        location.assign('https://archive.is/?run=1&url=' + location.href);
+    }
+})();
