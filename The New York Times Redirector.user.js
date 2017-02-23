ce7248108f361c6069aadc13aaa1c028e328b94c
Six <brbsix@gmail.com>
2017-02-22 18:03:35 -0800

    Significantly improve archive.is redirectors

--- The New York Times Redirector.user.js
+++ The New York Times Redirector.user.js
@@ -2,15 +2,48 @@
 // @name         The New York Times Redirector
 // @namespace    https://brbsix.github.io/
 // @version      0.1
-// @description  Bypass The New York Times paywall by redirecting to an archive of the page
 // @author       Brian Beffa <brbsix@gmail.com>
+// @description  Bypass The New York Times paywall by redirecting to an archive of the page
 // @match        https://myaccount.nytimes.com/auth/login?URI=*
+// @connect      archive.is
 // @run-at       document-start
-// @grant        none
+// @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    location.assign('https://archive.is/?run=1&url=' + encodeURIComponent(new URLSearchParams(location.search.substring(1)).get('URI').split('?')[0]));
+    var TM_DEBUG = 1;
+
+    function debug(msg) {
+        if (TM_DEBUG) {
+            console.log('Tampermonkey: ' + msg);
+        }
+    }
+
+    function getArchive(url) {
+        const host = 'https://archive.is';
+
+        GM_xmlhttpRequest({
+            method: 'GET',
+            url: host + '/timemap/' + url,
+            onload: function(response) {
+                debug('response.readyState: ' + response.readyState);
+                debug('response.responseHeaders: ' + response.responseHeaders);
+                debug('response.responseText: ' + response.responseText);
+                debug('response.status: ' + response.status);
+                debug('response.statusText: ' + response.statusText);
+                if (response.status === 200) {
+                    debug('opening existing snapshot');
+                    location.assign(host + '/timegate/' + url);
+                } else if (response.status === 404) {
+                    debug('creating snapshot');
+                    location.assign(host + '/?run=1&url=' + encodeURIComponent(url));
+                }
+            }
+        });
+    }
+
+    debug('detects paywalled NYT article');
+    getArchive(new URLSearchParams(window.location.search.substring(1)).get('URI').split('?')[0]);
 })();
--- The Wall Street Journal Redirector.user.js
+++ The Wall Street Journal Redirector.user.js
@@ -2,20 +2,53 @@
 // @name         The Wall Street Journal Redirector
 // @namespace    https://brbsix.github.io/
 // @version      0.1
-// @description  Bypass The Wall Street Journal paywall by redirecting to an archive of the page
 // @author       Brian Beffa <brbsix@gmail.com>
+// @description  Bypass The Wall Street Journal paywall by redirecting to an archive of the page
 // @match        http://www.wsj.com/articles/*
-// @grant        none
+// @connect      archive.is
+// @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
 (function() {
     'use strict';
 
+    var TM_DEBUG = 1;
+
+    function debug(msg) {
+        if (TM_DEBUG) {
+            console.log('Tampermonkey: ' + msg);
+        }
+    }
+
     function getMetaContent(name) {
         return document.querySelector(`meta[name='${name}']`).content;
     }
 
+    function getArchive(url) {
+        const host = 'https://archive.is';
+
+        GM_xmlhttpRequest({
+            method: 'GET',
+            url: host + '/timemap/' + url,
+            onload: function(response) {
+                debug('response.readyState: ' + response.readyState);
+                debug('response.responseHeaders: ' + response.responseHeaders);
+                debug('response.responseText: ' + response.responseText);
+                debug('response.status: ' + response.status);
+                debug('response.statusText: ' + response.statusText);
+                if (response.status === 200) {
+                    debug('opening existing snapshot');
+                    location.assign(host + '/timegate/' + url);
+                } else if (response.status === 404) {
+                    debug('creating snapshot');
+                    location.assign(host + '/?run=1&url=' + encodeURIComponent(url));
+                }
+            }
+        });
+    }
+
     if (getMetaContent('article.template') !== 'full') {
-        location.assign(`https://archive.is/?run=1&url=${location.href}`);
+        debug('detects paywalled WSJ article');
+        getArchive(window.location.href);
     }
 })();
