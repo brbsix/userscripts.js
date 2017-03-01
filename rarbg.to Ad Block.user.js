737e6c4e7d046f660d86449e5cad17aa53eeb753
Six <brbsix@gmail.com>
2017-03-01 06:54:22 -0800

    Refactor rarbg. script

--- rarbg.to Ad Block.user.js
+++ rarbg.to Ad Block.user.js
@@ -14,6 +14,10 @@
 
     const scriptName = GM_info.script.name;
 
+    function isObject(object) {
+            return typeof object === 'object';
+    }
+
     function log(msg) {
         if (msg) {
             GM_log(scriptName + ':', msg);
@@ -24,12 +28,36 @@
 
     log();
 
-    // _wm is initialized in https://dyncdn.me/static/20/js/expla5.js, where
-    // most of the popunder code is handled. _wm is used by other ad-related
-    // scripts also, so we want to prevent it from being used. The site checks
-    // whether _wm is undefined in order to determine if an ad blocker is being
-    // used, so all that we have to do is set it to something else (e.g. null).
+    // Most of rarbg's popunder code is defined in:
+    // https://dyncdn.me/static/20/js/expla5.js
+    //
+    // This is where _wm is initialized. Other ad-related scripts also
+    // utilize it, so we want to prevent it from being used. However if
+    // it is undefined, the site assumes that some sort of ad blocker is
+    // in use and responds accordingly. To get around this, we can set
+    // it to an empty object literal.
     //
     // NOTE: ensure expla5.js is not blocked by other ad-blocking software
-    _wm = null;
+    try {
+        if (typeof _wm === 'object') {
+            log('overwriting _wm');
+            _wm = {};
+        } else {
+            log('_wm is not an object');
+        }
+    } catch (e) {
+        log('error checking _wm');
+    }
+
+    // overwrite the popunder configuration (probably not necessary)
+    try {
+        if (typeof _wm_settings === 'object') {
+            log('overwriting _wm_settings');
+            _wm_settings = {};
+        } else {
+            log('_wm_settings is not an object');
+        }
+    } catch (e) {
+        log('error checking _wm_settings');
+    }
 })();
