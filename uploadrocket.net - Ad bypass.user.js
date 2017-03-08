bc2099eff12f3d7b4d1139ecd7f3405c1cfc6e12
Six <brbsix@gmail.com>
2017-03-08 10:40:16 -0800

    Hide a lot more, block nefarious activity, copy download link to clipboard

--- uploadrocket.net - Ad bypass.user.js
+++ uploadrocket.net - Ad bypass.user.js
@@ -7,8 +7,11 @@
 // @match        http://uploadrocket.net/*
 // @match        http://uploadrocket.net/*
 // @run-at       document-end
+// @grant        GM_addStyle
 // @grant        GM_info
 // @grant        GM_log
+// @grant        GM_setClipboard
+// @grant        GM_unsafeWindow
 // ==/UserScript==
 
 (function () {
@@ -24,7 +27,68 @@
         }
     }
 
-    // click free download button
+    // defuse Ad Maven
+    // https://github.com/AdguardTeam/AdguardFilters/blob/master/EnglishFilter/sections/general_extensions.txt)
+    // https://forum.adguard.com/index.php?threads/resolved-onwatchseries-to.18912/
+    Object.defineProperties(unsafeWindow, {
+        admvpu: {
+            get: function() {
+                log('something attempted to access window.admvpu');
+            },
+            set: function(value) {
+                if (typeof value === 'function') {
+                    log('something attempted to set window.admvpu to a function');
+                    throw Error();
+                } else {
+                    log('something attempted to set window.admvpu to something other than a function');
+                }
+            }
+        },
+        Fingerprint2: {
+            get: function() {
+                log('something attempted to access window.Fingerprint2');
+            },
+            set: function() {
+                log('something attempted to set window.Fingerprint2');
+                throw Error();
+            }
+        }
+    });
+
+    // defuse window.open
+    Object.defineProperty(unsafeWindow, 'open', {
+        get: function () {
+            log('something attempted to access window.open');
+            return function (url) {
+                log(`window.open blocked an attempt to open ${url}`);
+                return new Proxy({}, {
+                    get: function(target, name) {
+                        log(`dummy window logged access to property '${name}'`);
+                    },
+                    set: function(target, name, value) {
+                        log(`dummy window logged setting of property '${name}' to ${value}`);
+                    }
+                });
+            };
+        },
+        set: function () {
+            log('something attempted to redefine window.open');
+        }
+    });
+
+    // defuse document.onclick
+    // https://forum.adguard.com/index.php?threads/resolved-uploadrocket-net.12845/
+    Object.defineProperty(document, 'onclick', {
+        get: function () {
+            log('attempting to access to value of document.onclick');
+        }
+    });
+
+    // hide glasstop overlay (normally performed by breakglass() upon click)
+    log('editing style sheet to hide glasstop overlay');
+    GM_addStyle('#glasstop { display: none; }');
+
+    // click free download button (1st page)
     const freeBtn = document.querySelector('input[type="submit"][name="method_isfree"]');
     if (freeBtn !== null) {
         log('clicking free download button');
@@ -32,27 +96,73 @@
         return;
     }
 
-    // scroll to bottom of page for captcha entry
+    // assist with captcha (2nd page)
     const captchaBtn = document.querySelector('input[type="submit"][id="btn_download"][value="Create link from captcha code"]');
     if (captchaBtn !== null) {
+
+        const premiumJunk = document.querySelector('div[style="z-index:2001;position:relative;font-size: 24px;"]');
+        if (premiumJunk !== null) {
+            log('removing premium upsell');
+            premiumJunk.style.display = 'none';
+        }
+
+        const blogJunk = document.querySelector('table.file_slot');
+        if (blogJunk !== null) {
+            log('removing blog entries on captcha entry page');
+            blogJunk.style.display = 'none';
+        }
+
         log('scrolling to bottom of page for captcha entry');
         window.scrollTo(0, document.body.scrollHeight);
+
+        // this isn't particularly reliable
+        const solverBtn = document.querySelector('#play_button');
+        if (solverBtn !== null) {
+            log('clicking play button on Solve Media captcha');
+            solverBtn.click();
+        }
 
         const solverInput = document.querySelector('input[type="text"][name="adcopy_response"][id="adcopy_response"]');
         if (solverInput !== null) {
             log('focusing on Solve Media captcha input');
             solverInput.focus();
         }
+
         return;
     }
 
-    // automatically begin download
+    // assist with download (3rd page)
     const directLink = Array.from(document.querySelectorAll('a[href*="/d/"]')).find(function (l) {
         return l.textContent === 'Direct Download Link (No ads)';
     });
     if (typeof directLink !== 'undefined') {
-        log('clicking direct download link');
-        window.location.replace(directLink.href);
+        const headerJunk = document.querySelector('table.file_slot:nth-of-type(1)');
+        if (headerJunk !== null) {
+            log('removing annoying header on download page');
+            headerJunk.style.display = 'none';
+        }
+
+        const blogJunk = document.querySelector('table.file_slot:nth-of-type(3)');
+        if (blogJunk !== null) {
+            log('removing blog entries on download page');
+            blogJunk.style.display = 'none';
+        }
+
+        const premiumBtn = document.querySelector('input[type="submit"][name="method_ispremium"][value="Premium Download"]');
+        if (premiumBtn !== null) {
+            log('removing premium download button on download page');
+            premiumBtn.parentElement.parentElement.style.display = 'none';
+
+            const spacerElement = document.querySelector('td[style="width:5%;"]');
+            if (spacerElement !== null) {
+                log('removing spacer element on download page');
+                spacerElement.style.display = 'none';
+            }
+        }
+
+        log('copying direct download link to clipboard');
+        GM_setClipboard(directLink.href);
+
         return;
     }
 
