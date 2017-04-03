d03ce3a0f7547b60c15a505df5808afc0bb7b0c7
Six <brbsix@gmail.com>
2017-04-03 01:42:06 -0700

    Initial commit

---
+++ Ad Overlay Bypass.user.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @name         Ad Overlay Bypass
+// @namespace    https://brbsix.github.io/
+// @version      0.1
+// @author       Brian Beffa <brbsix@gmail.com>
+// @description  Bypass ad overlays found on some mature video hosts
+// @match        http://www.gameofporn.net/video/*
+// @match        http://www.hclips.com/videos/*
+// @match        https://www.ceporn.net/video/*
+// @updateURL    https://github.com/brbsix/userscripts.js/raw/master/Ad%20Overlay%20Bypass.user.js
+// @grant        GM_info
+// @grant        GM_log
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    function log () {
+        GM_log(...[arguments.length ? GM_info.script.name + ':' : GM_info.script.name, ...arguments]);
+    }
+
+    function hideAdOverlay () {
+        const btnName = 'Close & Play';
+        const btnSelector = new Map([
+            ['www.ceporn.net', '#vad > div:nth-child(2) > div:nth-child(2)'],
+            ['www.hclips.com', '.pl_adv1_c.ind_close'],
+            ['www.gameofporn.net', '.video-banner-close-play']
+        ]).get(window.location.host);
+
+        if (typeof btnSelector === 'undefined') {
+            throw Error(`No selector for "${btnName}" button on the current host`);
+        }
+
+        const btn = document.querySelector(btnSelector);
+
+        if (typeof btn === null) {
+            throw Error(`"${btnName}" button was not found`);
+        }
+
+        log(`clicking "${btnName}" button`);
+        btn.click();
+    }
+
+    log();
+
+    hideAdOverlay();
+})();
