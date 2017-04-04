179a49091834ad28b74aba49eed93f43fd6928c8
Six <brbsix@gmail.com>
2017-04-04 01:50:00 -0700

    Initial commit

---
+++ Hacker News - disable flagging.user.js
@@ -0,0 +1,42 @@
+// ==UserScript==
+// @name         Hacker News - disable flag links
+// @namespace    https://brbsix.github.io/
+// @version      0.1
+// @author       Brian Beffa <brbsix@gmail.com>
+// @description  Require double click to re-enable flag link
+// @match        https://news.ycombinator.com/*
+// @icon         https://news.ycombinator.com/favicon.ico
+// @updateURL    https://github.com/brbsix/userscripts.js/raw/master/Hacker%20News%20-%20disable%20flagging.user.js
+// @grant        GM_log
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    function disableLink (element) {
+        element.removeAttribute('href');
+        element.innerHTML = `<del>${element.textContent}</del>`;
+    }
+
+    function enableLink (element, outerHTML) {
+        element.outerHTML = outerHTML;
+        log('enabled element');
+    }
+
+    function log () {
+        GM_log(...[arguments.length ? GM_info.script.name + ':' : GM_info.script.name, ...arguments]);
+    }
+
+    log('configuring flag links');
+
+    Array.from(
+        document.querySelectorAll('a[href^="flag?"]')
+    ).forEach(
+        l => {
+            const outerHTML = l.outerHTML;
+            disableLink(l);
+            l.ondblclick = () => enableLink(l, outerHTML);
+        }
+    );
+
+})();
