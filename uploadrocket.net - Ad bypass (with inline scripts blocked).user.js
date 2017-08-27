0091ebb822c6b4ac51836b4a5f58eda7bdb52b70
Six <brbsix@gmail.com>
2017-08-27 02:53:23 -0700

    Fix updateURL

--- uploadrocket.net - Ad bypass (with inline scripts blocked).user.js
+++ uploadrocket.net - Ad bypass (with inline scripts blocked).user.js
@@ -9,7 +9,7 @@
 // @match        http://api.solvemedia.com/papi/media?*
 // @match        https://api.solvemedia.com/papi/media?*
 // @icon         http://uploadrocket.net/favicon.ico
-// @updateURL    https://github.com/brbsix/userscripts.js/raw/master/uploadrocket.net%20-%20Ad%20bypass.user.js
+// @updateURL    https://github.com/brbsix/userscripts.js/raw/master/uploadrocket.net%20-%20Ad%20bypass%20(with%20inline%20scripts%20blocked).user.js
 // @grant        GM_addStyle
 // @grant        GM_log
 // @grant        GM_notification
